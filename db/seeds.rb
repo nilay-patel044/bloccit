6ddfd4e10ca20bcdf5699d456870ea9b9495b4c2
nilay-patel044 <nilaypatel044@gmail.com>
2017-11-06 17:55:35 -0500

    Assignment-16: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,10 @@ posts = Post.all
   )
 end
 
+puts "#{Post.count} posts created"
+Post.find_or_create_by(title: "Unique Title", body: "Unique Body")
+puts "#{Post.count} posts created"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
