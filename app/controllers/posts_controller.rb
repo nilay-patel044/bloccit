773e3b9fec025ca0f0407ea26c364516ea773dde
nilay-patel044 <nilaypatel044@gmail.com>
2017-11-06 20:10:38 -0500

    Assignment-17: Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
-     @posts = Post.all    
+     @posts = Post.all
+     @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
+    # (0..@posts.size).step(5) { |5| @posts[5] @posts.title = "SPAM" }
   end
 
   def show
