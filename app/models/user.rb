65ba4ed65d1039f68c87d4c18d7aa2a874e35877
nilay-patel044 <nilaypatel044@gmail.com>
2017-11-15 02:25:19 -0500

    Assignment-23: Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
 
+  before_save { self.name = name.split.map(&:capitalize).join(" ") if name.present?}
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
@@ -11,5 +12,5 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
 
-  has_secure_password  
+  has_secure_password
 end
