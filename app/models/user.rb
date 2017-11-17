d0a7cafc76ce3b728cd8adfc5a6a13f558c9ece0
nilay-patel044 <nilaypatel044@gmail.com>
2017-11-17 00:26:21 -0500

    Checkpoint-26: Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
   before_save { self.email = email.downcase if email.present? }
 
 
@@ -11,5 +12,5 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
 
-  has_secure_password  
+  has_secure_password
 end
