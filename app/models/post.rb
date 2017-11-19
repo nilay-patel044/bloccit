ce4fb6453391069ebf50756d87a7cc7d08028eb5
nilay-patel044 <nilaypatel044@gmail.com>
2017-11-18 22:06:06 -0500

    Assignment-26: Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
 
   default_scope { order('created_at DESC') }
 
+  scope :ordered_by_title, -> { order('title DESC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_a').reverse}
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
