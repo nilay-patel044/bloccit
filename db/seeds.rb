54bcaec5be98b1804b4fc22a04aa3b491d7d104b
nilay-patel044 <nilaypatel044@gmail.com>
2017-11-20 23:50:56 -0500

    Checkpoint-28: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,27 @@
 require 'random_data'
 
-# Create Users
-5.times do
-  User.create!(
-# #3
-  name:     RandomData.random_name,
-  email:    RandomData.random_email,
-  password: RandomData.random_sentence
-  )
-end
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld',
+  role:     'member'
+)
+
+moderator = User.create!(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld',
+  role:     'moderator'
+)
 users = User.all
 
-# Create Topics
 15.times do
   Topic.create!(
     name:         RandomData.random_sentence,
@@ -20,11 +30,8 @@ users = User.all
 end
 topics = Topic.all
 
-# Create Posts
 50.times do
-# #1
   Post.create!(
-# #2
     user: users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
@@ -33,38 +40,14 @@ topics = Topic.all
 end
 posts = Post.all
 
-# Create Comments
-# #3
 100.times do
   Comment.create!(
-    user: users.sample,    
-# #4
+    user: users.sample,
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
 
-# Create an admin user
-admin = User.create!(
-  name:     'Admin User',
-  email:    'admin@example.com',
-  password: 'helloworld',
-  role:     'admin'
-)
-
-# Create a member
-member = User.create!(
-  name:     'Member User',
-  email:    'member@example.com',
-  password: 'helloworld'
-)
-
-# Create a moderator
-moderator = User.create!(
-  name:     'Moderator User',
-  email:    'moderator@gmail.com',
-  password: 'helloworld'
-)
 
 puts "Seed finished"
 puts "#{User.count} users created"
