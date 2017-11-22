22665b6296deed1fa5aa63a3b68387b81d34f5fd
nilay-patel044 <nilaypatel044@gmail.com>
2017-11-22 16:52:27 -0500

    Checkpoint-29: Completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 # #34
     resources :posts, except: [:index]
   end
-  
+
 # #4
   resources :posts, only: [] do
 # #5
     resources :comments, only: [:create, :destroy]
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote    
   end
 
   resources :users, only: [:new, :create]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120062542) do
+ActiveRecord::Schema.define(version: 20171122205845) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20171120062542) do
     t.datetime "updated_at", null: false
     t.integer "topic_id"
     t.integer "user_id"
+    t.float "rank"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
@@ -59,6 +60,16 @@ ActiveRecord::Schema.define(version: 20171120062542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "value"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_votes_on_post_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
